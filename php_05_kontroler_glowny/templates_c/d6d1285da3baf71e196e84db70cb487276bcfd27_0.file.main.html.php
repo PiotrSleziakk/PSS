913494bae982a90d69360af8a1f63e83676a401d
Piotr Śleziak <101433377+PiotrSleziakk@users.noreply.github.com>
2024-04-18 12:43:12 +0200

    php_05a_kontroler_glowny

---
+++ php_05_kontroler_glowny/templates_c/d6d1285da3baf71e196e84db70cb487276bcfd27_0.file.main.html.php
@@ -0,0 +1,160 @@
+<?php
+/* Smarty version 4.3.2, created on 2024-04-18 12:39:26
+  from 'D:\xampp\htdocs\php_05_kontroler_glowny\templates\main.html' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.3.2',
+  'unifunc' => 'content_6620f85ecf6744_14107621',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'd6d1285da3baf71e196e84db70cb487276bcfd27' => 
+    array (
+      0 => 'D:\\xampp\\htdocs\\php_05_kontroler_glowny\\templates\\main.html',
+      1 => 1713114093,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_6620f85ecf6744_14107621 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_loadInheritance();
+$_smarty_tpl->inheritance->init($_smarty_tpl, false);
+?>
+<!DOCTYPE HTML>
+<!--
+	Twenty by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html lang="pl">
+<head>
+    <title><?php echo (($tmp = $_smarty_tpl->tpl_vars['page_title']->value ?? null)===null||$tmp==='' ? "Tytuł domyślny" ?? null : $tmp);?>
+</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <link rel="stylesheet" href="http://localhost:80/php_01_widok_kontroler/assets/css/main.css" />
+    <noscript><link rel="stylesheet" href="http://localhost:80/php_01_widok_kontroler/assets/css/noscript.css" /></noscript>
+</head>
+<body class="contact is-preload">
+    <div id="page-wrapper">
+
+        <!-- Header -->
+        <header id="header">
+            <h1 id="logo">Minima_/Credic</h1>
+            <nav id="nav">
+                <ul>
+                    <li><a href="#" class="button primary">Login</a></li>
+                </ul>
+            </nav>
+        </header>
+
+        <!-- Main -->
+        <article id="main">
+
+            <header class="special container">
+                    <h2>kalkulator kredytowy</h2>
+            </header>
+
+            <!-- One -->
+            <section class="wrapper style4 special container medium">
+
+                 <!-- Content -->
+                <div class="content">
+                    <?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_3354412236620f85ecf60f7_45138499', 'content');
+?>
+
+                </div>
+
+            </section>
+
+        </article>
+
+        <!-- Footer -->
+        <footer id="footer">
+
+            <ul class="icons">
+                <li><a href="#" class="icon brands circle fa-twitter"><span class="label">Twitter</span></a></li>
+                <li><a href="#" class="icon brands circle fa-facebook-f"><span class="label">Facebook</span></a></li>
+                <li><a href="#" class="icon brands circle fa-google-plus-g"><span class="label">Google+</span></a></li>
+                <li><a href="#" class="icon brands circle fa-github"><span class="label">Github</span></a></li>
+                <li><a href="#" class="icon brands circle fa-dribbble"><span class="label">Dribbble</span></a></li>
+            </ul>
+
+            <ul class="copyright">
+                <li>&copy; Untitled</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+            </ul>
+
+        </footer>
+
+        </div>
+
+            <!-- Scripts -->
+            <?php echo '<script'; ?>
+ src="<?php echo '<?php'; ?>
+ print(_APP_ROOT); <?php echo '?>'; ?>
+assets/js/jquery.min.js"><?php echo '</script'; ?>
+>
+            <?php echo '<script'; ?>
+ src="<?php echo '<?php'; ?>
+ print(_APP_ROOT); <?php echo '?>'; ?>
+assets/js/jquery.dropotron.min.js"><?php echo '</script'; ?>
+>
+            <?php echo '<script'; ?>
+ src="<?php echo '<?php'; ?>
+ print(_APP_ROOT); <?php echo '?>'; ?>
+assets/js/jquery.scrolly.min.js"><?php echo '</script'; ?>
+>
+            <?php echo '<script'; ?>
+ src="<?php echo '<?php'; ?>
+ print(_APP_ROOT); <?php echo '?>'; ?>
+assets/js/jquery.scrollgress.min.js"><?php echo '</script'; ?>
+>
+            <?php echo '<script'; ?>
+ src="<?php echo '<?php'; ?>
+ print(_APP_ROOT); <?php echo '?>'; ?>
+assets/js/jquery.scrollex.min.js"><?php echo '</script'; ?>
+>
+            <?php echo '<script'; ?>
+ src="<?php echo '<?php'; ?>
+ print(_APP_ROOT); <?php echo '?>'; ?>
+assets/js/browser.min.js"><?php echo '</script'; ?>
+>
+            <?php echo '<script'; ?>
+ src="<?php echo '<?php'; ?>
+ print(_APP_ROOT); <?php echo '?>'; ?>
+assets/js/breakpoints.min.js"><?php echo '</script'; ?>
+>
+            <?php echo '<script'; ?>
+ src="<?php echo '<?php'; ?>
+ print(_APP_ROOT); <?php echo '?>'; ?>
+assets/js/util.js"><?php echo '</script'; ?>
+>
+            <?php echo '<script'; ?>
+ src="<?php echo '<?php'; ?>
+ print(_APP_ROOT); <?php echo '?>'; ?>
+assets/js/main.js"><?php echo '</script'; ?>
+>
+
+</body>
+</html><?php }
+/* {block 'content'} */
+class Block_3354412236620f85ecf60f7_45138499 extends Smarty_Internal_Block
+{
+public $subBlocks = array (
+  'content' => 
+  array (
+    0 => 'Block_3354412236620f85ecf60f7_45138499',
+  ),
+);
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+ Domyślna treść zawartości .... <?php
+}
+}
+/* {/block 'content'} */
+}

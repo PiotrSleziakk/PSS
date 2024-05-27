07ddff95d80628267d2070305704a92f5c4eb1aa
Piotr Śleziak <101433377+PiotrSleziakk@users.noreply.github.com>
2024-05-27 20:36:21 +0200

    Add files via upload

---
+++ php_07_bd/templates_c/43e0f72d91ac1d67a75cc4533c7cc09ac60e9bd8_0.file.PersonList.tpl.php
@@ -0,0 +1,115 @@
+<?php
+/* Smarty version 3.1.30, created on 2024-05-27 20:19:18
+  from "D:\xampp\htdocs\php_08_bd\app\views\PersonList.tpl" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_6654cea67c2584_81272124',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '43e0f72d91ac1d67a75cc4533c7cc09ac60e9bd8' => 
+    array (
+      0 => 'D:\\xampp\\htdocs\\php_08_bd\\app\\views\\PersonList.tpl',
+      1 => 1716833628,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+    'file:main.tpl' => 1,
+  ),
+),false)) {
+function content_6654cea67c2584_81272124 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_loadInheritance();
+$_smarty_tpl->inheritance->init($_smarty_tpl, true);
+?>
+
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_18516730006654cea67bbf52_83331835', 'top');
+?>
+
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_11142085436654cea67c2093_01214112', 'bottom');
+?>
+
+<?php $_smarty_tpl->inheritance->endChild();
+$_smarty_tpl->_subTemplateRender("file:main.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 2, false);
+}
+/* {block 'top'} */
+class Block_18516730006654cea67bbf52_83331835 extends Smarty_Internal_Block
+{
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+
+
+<div class="bottom-margin">
+<form class="pure-form pure-form-stacked" action="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_url;?>
+personList">
+	<legend>Opcje wyszukiwania</legend>
+	<fieldset>
+		<input type="text" placeholder="nazwisko" name="sf_surname" value="<?php echo $_smarty_tpl->tpl_vars['searchForm']->value->surname;?>
+" /><br />
+		<button type="submit" class="pure-button pure-button-primary">Filtruj</button>
+	</fieldset>
+</form>
+</div>	
+
+<?php
+}
+}
+/* {/block 'top'} */
+/* {block 'bottom'} */
+class Block_11142085436654cea67c2093_01214112 extends Smarty_Internal_Block
+{
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+
+
+<div class="bottom-margin">
+<a class="pure-button button-success" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_root;?>
+personNew">+ Nowa osoba</a>
+</div>	
+
+<table id="tab_people" class="pure-table pure-table-bordered">
+<thead>
+	<tr>
+		<th>imię</th>
+		<th>nazwisko</th>
+		<th>data ur.</th>
+		<th>nr. telefonu</th>
+		<th>opcje</th>
+	</tr>
+</thead>
+<tbody>
+<?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['people']->value, 'p');
+if ($_from !== null) {
+foreach ($_from as $_smarty_tpl->tpl_vars['p']->value) {
+?>
+<tr><td><?php echo $_smarty_tpl->tpl_vars['p']->value["name"];?>
+</td><td><?php echo $_smarty_tpl->tpl_vars['p']->value["surname"];?>
+</td><td><?php echo $_smarty_tpl->tpl_vars['p']->value["birthdate"];?>
+</td><td><?php echo $_smarty_tpl->tpl_vars['p']->value["phone"];?>
+</td><td><a class="button-small pure-button button-secondary" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_url;?>
+personEdit&id=<?php echo $_smarty_tpl->tpl_vars['p']->value['idperson'];?>
+">Edytuj</a>&nbsp;<a class="button-small pure-button button-warning" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_url;?>
+personDelete&id=<?php echo $_smarty_tpl->tpl_vars['p']->value['idperson'];?>
+">Usuń</a></td></tr>
+<?php
+}
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl);
+?>
+
+</tbody>
+</table>
+
+<?php
+}
+}
+/* {/block 'bottom'} */
+}
---
+++ php_07_bd/templates_c/e849f47073914936fb4df098330d5bf7a52380de_0.file.LoginView.tpl.php
@@ -0,0 +1,65 @@
+<?php
+/* Smarty version 3.1.30, created on 2024-05-27 20:33:09
+  from "D:\xampp\htdocs\php_07_bd\app\views\LoginView.tpl" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_6654d1e53a7d14_49832152',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'e849f47073914936fb4df098330d5bf7a52380de' => 
+    array (
+      0 => 'D:\\xampp\\htdocs\\php_07_bd\\app\\views\\LoginView.tpl',
+      1 => 1716485512,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+    'file:main.tpl' => 1,
+  ),
+),false)) {
+function content_6654d1e53a7d14_49832152 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_loadInheritance();
+$_smarty_tpl->inheritance->init($_smarty_tpl, true);
+?>
+
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_20082547376654d1e53a7a29_56439159', 'top');
+?>
+
+<?php $_smarty_tpl->inheritance->endChild();
+$_smarty_tpl->_subTemplateRender("file:main.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 2, false);
+}
+/* {block 'top'} */
+class Block_20082547376654d1e53a7a29_56439159 extends Smarty_Internal_Block
+{
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+
+<form action="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_root;?>
+login" method="post" class="pure-form pure-form-aligned bottom-margin">
+	<legend>Logowanie do systemu</legend>
+	<fieldset>
+        <div class="pure-control-group">
+			<label for="id_login">login: </label>
+			<input id="id_login" type="text" name="login" value="<?php echo $_smarty_tpl->tpl_vars['form']->value->login;?>
+"/>
+		</div>
+        <div class="pure-control-group">
+			<label for="id_pass">pass: </label>
+			<input id="id_pass" type="password" name="pass" /><br />
+		</div>
+		<div class="pure-controls">
+			<input type="submit" value="zaloguj" class="pure-button pure-button-primary"/>
+		</div>
+	</fieldset>
+</form>	
+<?php
+}
+}
+/* {/block 'top'} */
+}
